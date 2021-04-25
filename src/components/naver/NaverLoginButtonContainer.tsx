d460a328f445b17215be6163a04c2b9c3f7a9144
LovelyHaRa <codetender94@gmail.com>
2021-04-25 13:24:12 +0900

    feat: support popup window to naver login

--- src/components/naver/NaverLoginButtonContainer.tsx
+++ src/components/naver/NaverLoginButtonContainer.tsx
@@ -9,6 +9,10 @@ type Props = {
   redirectUri?: string;
   /** state를 지정할 떄 사용합니다 */
   state?: string;
+  /** popup창으로 열때 지정합니다 */
+  isPopupOpen?: boolean;
+  /** popup창의 windowFeatures를 지정합니다  */
+  popupWindowFeature?: string;
 };
 
 /** 네이버 아이디로 로그인 버튼 */
@@ -16,10 +20,16 @@ export const NaverLoginButtonContainer = ({
   clientId = "",
   redirectUri = "",
   state = "",
+  isPopupOpen = false,
+  popupWindowFeature = "width=500, height=700, status",
 }: Props) => {
   const handleClick = useCallback(() => {
     const requestUrl = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&state=${state}`;
-    window.open(requestUrl, "_blank");
+    if (isPopupOpen) {
+      window.open(requestUrl, "_blank", popupWindowFeature);
+    } else {
+      window.location.href = requestUrl;
+    }
   }, []);
 
   return <NaverLoginButton handleClick={handleClick} />;
