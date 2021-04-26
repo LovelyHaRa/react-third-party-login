b57aa6cd6b0d0fb3f67db9cd1eb594355dc935f2
LovelyHaRa <codetender94@gmail.com>
2021-04-27 00:06:40 +0900

    feat: support popup window to kakao login

--- src/components/kakao/KakaoLoginButtonContainer.tsx
+++ src/components/kakao/KakaoLoginButtonContainer.tsx
@@ -9,16 +9,26 @@ type Props = {
   redirectUri?: string;
   /** state를 지정할 떄 사용합니다 */
   state?: string;
+  /** popup창으로 열때 지정합니다 */
+  isPopupOpen?: boolean;
+  /** popup창의 windowFeatures를 지정합니다  */
+  popupWindowFeature?: string;
 };
 
 const KakaoLoginButtonContainer = ({
   clientId = "",
   redirectUri = "",
   state = "",
+  isPopupOpen = false,
+  popupWindowFeature = "width=500, height=750, status",
 }: Props) => {
   const handleClick = useCallback(() => {
     const requestUrl = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&state=${state}`;
-    window.open(requestUrl, "_blank");
+    if (isPopupOpen) {
+      window.open(requestUrl, "_blank", popupWindowFeature);
+    } else {
+      window.location.href = requestUrl;
+    }
   }, []);
 
   return <KakaoLoginButton handleClick={handleClick} />;
